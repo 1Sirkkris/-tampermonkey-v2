702f8ea99b6c2f9527ba072d37ba8d8de6c3207b
1Sirkkris <toffer100@gmail.com>
2026-09-21 12:10:58 +1000

    fix(sideline): suppress standalone Lazy veil inside ISS Console

--- Sideline_API_Move.user.js
+++ Sideline_API_Move.user.js
@@ -2,7 +2,7 @@
 // @name         MAIN v0.3.16 Sideline API Move TEST
 // @name:en      MAIN Sideline API Move TEST
 // @namespace    https://github.com/1Sirkkris
-// @version      0.3.32
+// @version      0.3.33
 // @description  Sideline helper: Tote, Scrub, QTY, Lazy and Live workflows.
 // @match        https://aft-poirot-website-nrt.nrt.proxy.amazon.com/*
 // @include      /^https?:\/\/.*fcresearch.*\//
@@ -29,7 +29,7 @@
   const ISS_WORKER_BY_QUERY = new URLSearchParams(location.search).get('issConsoleWorker') === '1';
   const ISS_WORKER_BY_NAME = window.name === 'iss-console-sideline-worker';
   const ISS_CONSOLE_WORKER = ISS_CONSOLE_LOCAL || ISS_WORKER_BY_HASH || ISS_WORKER_BY_QUERY || ISS_WORKER_BY_NAME;
-  const VERSION = '0.3.32';
+  const VERSION = '0.3.33';
   const POIROT_ORIGIN = 'https://aft-poirot-website-nrt.nrt.proxy.amazon.com';
 
   function startRuntime() {
@@ -3570,6 +3570,11 @@
 
   function setLazyRunningIndicator(on) {
     let el = $('#sh-lazy-running-indicator');
+
+    if (ISS_CONSOLE_LOCAL) {
+      el?.remove();
+      return;
+    }
 
     if (on) {
       if (!el) {
